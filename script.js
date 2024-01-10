173d8567e0c8364dfa7b95155af6f3b8f306f041
Niete <nieteis@gmail.com>
2024-01-10 19:04:25 +0000

    Added html, css and javascript code that allows you to open and close nav menu

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+/*==================SHOW MENU=================*/
+
+if(navToggle){
+    navToggle.addEventListener('click',() =>{
+        navMenu.classList.add('show-menu');
+    })
+}
+
+/*==================HIDE MENU=================*/
+
+if (navClose){
+    navClose.addEventListener('click',() =>{
+        navMenu.classList.remove('show-menu');
+})
+}
