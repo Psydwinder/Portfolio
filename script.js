ab9e5242339868cb0c42e16f3d8a869f40b7870f
Niete <nieteis@gmail.com>
2024-01-31 23:49:22 +0000

    Added JS code to send email to my inbox from contact form

--- script.js
+++ script.js
@@ -27,3 +27,34 @@ const linkAction = () => {
 }
 
 navLink.forEach(n => n.addEventListener('click',linkAction))
+
+/*==================EMAIL JS=================*/
+
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    //serviceID - templateID - #form -publicKey
+    emailjs.sendForm('service_l46fhs8','template_s19ix7t', '#contact-form', '6tOQu3mMOIuLVK2hk')
+    .then(() => {
+        //show sent message
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        //Remove message after five seconds
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 5000)
+
+        //Clear input fields
+        contactForm.reset()
+    }, () => {
+       // show error message 
+       contactMessage.textContent = 'Message not sent (service error) ❌'
+
+    }) 
+    
+}
+
+contactForm.addEventListener('submit', sendEmail); 
